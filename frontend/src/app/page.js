8f85ce665153b50525c1fecd432e4f9dfb194afa
sss2611 <solss2611@gmail.com>
2025-07-17 17:26:27 -0300

    empezar desde aqui

--- frontend/src/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-purple-100">
-      <h1 className="text-3xl font-bold text-purple-700">
-        ¡Tailwind está vivo y funcionando! 🐾
-      </h1>
-    </main>
-  );
-}
