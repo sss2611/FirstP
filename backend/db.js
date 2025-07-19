6635166de591d1a7da4799545d41f7d803f85d59
sss2611 <solss2611@gmail.com>
2025-07-19 16:50:20 -0300

    todo eliminado

--- backend/db.js
+++
@@ -1,4 +0,0 @@
-let productos = [
-  { id: 1, nombre: "Zapatillas", precio: 12000, img: "zapatillas.jpg", descripcion: "Cómodas para correr" },
-  { id: 2, nombre: "Remera", precio: 5000, img: "remera.jpg", descripcion: "Algodón suave" }
-];
