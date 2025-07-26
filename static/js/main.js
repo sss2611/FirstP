88074605b2b5b2d01f51b9ab901d4203717b1120
sss2611 <solss2611@gmail.com>
2025-07-26 02:14:51 -0300

    elección de temas para cambiar el estilo de acuerdo al usuario

--- static/js/main.js
+++ static/js/main.js
@@ -208,3 +208,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// Se adapta al tema elegido en el dashboard
+const savedTheme = localStorage.getItem("selectedTheme");
+const themeLink = document.getElementById("theme-link");
+
+if (savedTheme && themeLink) {
+  themeLink.href = `https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/${savedTheme}/bootstrap.min.css`;
+}
